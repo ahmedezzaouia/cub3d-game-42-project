5aaf18fd2728731df454054840ce4f005d83a273
Mohammed Abdelouafi <mabdelou@e1r13p7.1337.ma>
2022-08-26 11:53:16 +0100

    hello
    world

---
+++ push_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo remove cache files\n
+
+rm -rf . ; rm -rf ./. ; rm 1 2 3 ; rm -rf ././.
+
+echo git add
+    git add .
+echo git commit
+    git commit -m $1 -m $2
+echo git push
+    git push
